82989e49b3eae416936d92b34b2c6d60c6d14441
Maxim Nikolyukin <ch1ppyone@mail.ru>
2021-09-24 11:15:18 +0300

    Валидация

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -150,3 +150,50 @@ function getCardElement(name, link) {
 function renderCard(card, container) {
   container.prepend(card);
 }
+
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  //inputElement.classList.add('form__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__error_visible');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+ // inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('popup__error_visible');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+    });
+  });
+};
+
+
+const enableValidation = () => {
+  let formList = Array.from(document.querySelectorAll('.popup__form')); 
+  formList.forEach((formElement) => {
+  formElement.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+  });
+
+    setEventListeners(formElement);
+}); 
+};
+
+enableValidation();
